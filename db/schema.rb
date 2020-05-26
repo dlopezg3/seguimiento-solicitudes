148c6638e85e0f77bb8e6bc5a93c0c96b13633ae
Daniel Lopez <dlopez.10g@gmail.com>
2020-05-26 17:23:38 -0500

    table for cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_200840) do
+ActiveRecord::Schema.define(version: 2020_05_26_210316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "responsable"
+    t.string "solicitante"
+    t.string "nombre"
+    t.string "descripcion"
+    t.string "lista"
+    t.date "fecha_solicitud"
+    t.date "fecha_cambio"
+    t.string "lista_anterior"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
