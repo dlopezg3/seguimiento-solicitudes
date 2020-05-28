df377b126432cfe9d37c705a01445a20e7afe818
Daniel Lopez <dlopez.10g@gmail.com>
2020-05-28 14:10:39 -0500

    traer personas y listas dentro del tablero al controller de cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,8 @@
 class CardsController < ApplicationController
 
   def index
+    @members = User.fetch_trello_members
+    @lists = List.fetch_trello_list
     @cards = Card.sort_by_date(Card.fetch_trello_cards)
   end
 end
