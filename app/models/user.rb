de4df93ff768d57ffa4c4e6e7f8f4abe019766ff
Daniel Lopez <dlopez.10g@gmail.com>
2020-05-28 14:12:57 -0500

    fetch de miembros dentro del board

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.fetch_trello_members
+    board_id = "5ec56063edc3f53f0c17f299"
+    url = "https://api.trello.com/1/boards/#{board_id}/members?key=#{ENV['TRELLO_KEY']}&token=#{ENV['TRELLO_TOKEN']}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
 end
