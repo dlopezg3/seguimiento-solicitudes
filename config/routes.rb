4add5ffe52216dd70a2e65a18e802f255994a508
Daniel Lopez <dlopez.10g@gmail.com>
2020-06-01 17:38:11 -0500

    ruta url para el dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'board_stats/index'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'cards#index'
   resources :cards, only: :index
+  resources :board_stats, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
