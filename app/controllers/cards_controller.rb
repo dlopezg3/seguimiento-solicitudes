cbb3498e88199d98172ed5722b7422a9b935fcd3
Daniel Lopez <dlopez.10g@gmail.com>
2020-06-01 14:36:13 -0500

    Metodo para filtrar tarjetas en el controler de tarjetas

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,19 @@
 class CardsController < ApplicationController
 
   skip_before_action :authenticate_user!
+  before_action :set_filtered_cards
 
   def index
     @members = User.fetch_trello_members
     @lists = List.fetch_trello_list
+  end
+
+  private
+
+  def set_filtered_cards
     @cards = Card.sort_by_date(Card.fetch_trello_cards)
+    return @cards if params.empty?
+
+    @cards = Card.secretary_filter(@cards, params.slice(:secretaria)) unless params["secretaria"].nil?
   end
 end
