ec1a1bb0cd914df4dd8c612cbaa4d5e61c86a822
Daniel Lopez <dlopez.10g@gmail.com>
2020-06-01 17:39:50 -0500

    Controller para las estadisticas del tablero con acción de index y filtros

---
+++ app/controllers/board_stats_controller.rb
@@ -0,0 +1,25 @@
+class BoardStatsController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_filtered_cards
+
+  def index
+    @members = User.fetch_trello_members
+    @lists = List.fetch_trello_list
+  end
+
+  private
+
+  def set_filtered_cards
+    @cards = Card.sort_by_date(Card.fetch_trello_cards)
+    @secretarias = secretarias_array
+    return @cards if params.empty?
+
+    @cards = Card.secretary_filter(@cards, params.slice(:secretaria)) unless params["secretaria"].nil?
+  end
+
+  def secretarias_array
+    @cards.reject { |card| card["custom_info"]["Secretaría"].nil?}
+          .map!   { |card| card["custom_info"]["Secretaría"] }
+          .uniq
+  end
+end
