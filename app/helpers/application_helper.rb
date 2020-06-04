c25bd0c79fc4c33b1bfaa06f45f4744c8d4b88f1
Daniel Lopez <dlopez.10g@gmail.com>
2020-06-04 16:10:21 -0500

    & comun helper methods added to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def active_filter(params)
+    return "Secretarías" if params["secretaria"].nil?
+
+    params["secretaria"]
+  end
+
+  def list_for_card(card)
+    @lists.find {|key, value| key["id"] == card["idList"]}
+  end
 end
