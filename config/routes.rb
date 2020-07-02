bb2b3406abb3cedb83892f629f18bac58a5a8358
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-02 18:07:34 -0500

    Eliminar ruta de crear nuevo usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+
+  resource :users,
+      only: [:edit, :update, :destroy],
+      controller: 'devise/registrations',
+      as: :user_registration do
+    get 'cancel'
+  end
+
   root to: 'cards#index'
   resources :cards, only: :index
   resources :board_stats, only: :index
