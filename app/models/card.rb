89913105639ed3f2ca40d8466278706849cf6f6c
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-03 11:55:03 -0500

    Metodo para corregir el nombre de las secretarías

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,6 +5,9 @@ class Card < ApplicationRecord
     uri = URI(url)
     response = Net::HTTP.get(uri)
     cards_array = separate_description_array(JSON.parse(response))
+    cards_array.each do |card|
+      secretary_name(card)
+    end
   end
 
   def self.sort_by_date(cards_array)
@@ -34,5 +37,27 @@ class Card < ApplicationRecord
     description_item_array = element.split(":", 2)
     desc_hash["#{description_item_array.first.strip}"] = description_item_array.last.strip
     desc_hash
+  end
+
+  def self.secretary_name(card)
+    @secretary_name = card["custom_info"]["Secretaría"]
+    return if @secretary_name.nil?
+
+    @secretary_name = @secretary_name.titleize
+    convert_da if @secretary_name.include? ("Departamento Administrativo")
+    convert_dapard if @secretary_name.include? ("Dapard")
+    card["custom_info"]["Secretaría"] = @secretary_name
+  end
+
+  private
+
+  def self.convert_da
+    @secretary_name = @secretary_name.gsub("Departamento Administrativo", "D.A.")
+    @secretary_name
+  end
+
+  def self.convert_dapard
+    @secretary_name = @secretary_name.gsub("Dapard", "DAPARD")
+    @secretary_name
   end
 end
